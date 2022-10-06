26abcbb45cee47cf89187d4c14d07d52de8cf820
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-10-06 22:19:46 +0900

    Feat: useRouter 수정

--- src/hooks/useRouter.tsx
+++ src/hooks/useRouter.tsx
@@ -1,6 +1,13 @@
+import { ReactNode } from "react";
+
 const useRouter = () => {
-  const push = window.location.pathname;
-  return { push };
+  const path = window.location.pathname;
+  const push = (data: string, url: string) => {
+    window.history.pushState(data, "", url);
+    const popstate = new PopStateEvent("popstate");
+    window.dispatchEvent(popstate);
+  };
+  return { path, push };
 };
 
 export default useRouter;
