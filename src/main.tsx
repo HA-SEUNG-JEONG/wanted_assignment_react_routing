0d4969a16bae4d20162aa71cef14a33d08a893f6
HA-SEUNG-JEONG <gktmd652@naver.com>
2022-10-06 22:21:38 +0900

    Feat: Router,Route 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import Route from "./components/Route";
+import Router from "./components/Router";
+import About from "./pages/About";
+import Root from "./pages/Root";
 
 const container = document.getElementById("root") as HTMLElement;
 
 ReactDOM.createRoot(container).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route path="/" component={<Root />} />
+      <Route path="/about" component={<About />} />
+    </Router>
   </React.StrictMode>
 );
